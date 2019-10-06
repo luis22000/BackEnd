ea35baa6676f8440a507567cf3ebb570af7a9fcb
luis Pedro Gonzalez <40741277+luis22000@users.noreply.github.com>
2019-10-06 15:55:02 -0600

    Get y data

--- routes/Pelicula.js
+++ routes/Pelicula.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+let data = require('./data');
 
 router.get('/', function(req, res){
-    res.send('hello world');
+    res.status('200');
+    res.json(data);
 });
 
+router.get("/:pelicula", (req, res) => {
+    const NombrePelicula = req.params.pelicula;
+    const item = data.find(_item => _item.NombrePelicula === NombrePelicula);
+ 
+    if (item) {
+       res.status('200');
+       res.json(item);
+    } else {
+       res.status('404');
+       res.json({ message: `Pelicula ${NombrePelicula} No existe`})
+    }
+ });
+ 
 
 
 
